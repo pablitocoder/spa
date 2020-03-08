8bd8c26ceddb41f52dba023367ca20b6f7c62eb5
pablitocoder <35904158+pablitocoder@users.noreply.github.com>
2020-03-08 21:51:21 +0100

    update

--- 02-JSCoreConcepts/cars.js
+++ 02-JSCoreConcepts/cars.js
@@ -1,5 +1,12 @@
 function isCar(car1, car2, car3) {
-    // return 
+    let arr = [car1,car2,car3]
+    const alltrue = (val) => val==true;
+    const allfalse = (val) => val==false;
+    if(arr.every(alltrue) || arr.every(allfalse)){
+        return true
+    } else {
+        return false
+    }
 }
 
 console.log(isCar(true, true, true));
@@ -7,4 +14,3 @@ console.log(isCar(false, false, false));
 console.log(isCar(true, true, false));
 console.log(isCar(false, false, true));
 console.log(isCar(true, false, true));
-
