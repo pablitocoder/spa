3d7a1cc8590accd5c74a239331b1cc83f6f5743d
pablitocoder <35904158+pablitocoder@users.noreply.github.com>
2020-03-09 00:09:00 +0100

    Add files via upload
    ordering upload

--- 02-JSCoreConcepts/ordering.js
+++ 02-JSCoreConcepts/ordering.js
@@ -6,15 +6,12 @@ const reverse = function(arr) {
 }
 
 const sort = function(arr) {
-    arrSorted = [];
-    // ...
-    return arrSorted;
+    return arr.sort()
 }
 
 const even = function(arr) {
-    let arrEven = [];
-    // ...
-    return arrEven;
+    const isEven = (numb) => numb%2==0;
+    return arr.filter(isEven)
 }
 
 numbers = [8,3,2,1,5,4,6];
