b21382a8209f9fcf82b3a551f525dd2cd9e91199
pablitocoder <35904158+pablitocoder@users.noreply.github.com>
2020-03-09 00:52:31 +0100

    Add files via upload
    r

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -2,34 +2,32 @@ names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
 
 class Students {
     constructor(names) {
-        // ...
+        this.names = names
     }
 
     startWith(letter){
-        return // names (array) which starts with the letter
+        letter = letter.toUpperCase()
+        return this.names.filter((name)=>name.charAt(0)==letter)
     }
 
-    sort() {
-        return // names (array) in alphabetical order 
+    sort_n() {
+        return this.names.sort();
     }
 
     get() {
-        return // names (array) in natural order
+        return this.names
     }
 
-    getFirst(n) {
-        return // get first 'n' names (array) 
+    getFirst_n(n) {
+        return this.names.slice(0,n)
     }
 }
 
 students = new Students(names);
 console.log(students.get());
-console.log(students.sort());
+console.log(students.sort_n());
 console.log(students.startWith('P'));
-console.log(students.startWith('M').sort());
-console.log(students.sort().getFirst(4));
-console.log(students.startWith('M').sort().getFirst(2));
+console.log(students.startWith('M').sort_n());
+console.log(students.sort_n().getFirst_n(4));
+console.log(students.startWith('M').sort_n().getFirst_n(2));
 console.log(students.get());
-
-
-
